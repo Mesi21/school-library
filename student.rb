14159234a9e655f11b6090268be89a284d133d25
Mesi <mse212002@gmail.com>
2022-06-24 12:16:25 +0300

    :sparkles: set up associations

--- student.rb
+++ student.rb
@@ -8,6 +8,11 @@ class Student < Person
     @classroom = classroom
   end
 
+  def classroom(classroom)
+    @classroom = classroom
+    classroom.students_list.push(self) unless classroom.students_list.include?(self)
+  end
+
   def play_hookey
     "¯\(ツ)/¯"
   end
